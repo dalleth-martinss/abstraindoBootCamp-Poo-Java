d2e81aadd3d6797a1121a2ac737d5231c52e3179
dalleth-martinss <essi_dalleth@outlook.com>
2024-07-15 22:25:43 -0300

    Adding classes and methods

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.abstraindoBootcamp.desafio.dominio.Conteudo;
 import br.com.abstraindoBootcamp.desafio.dominio.Curso;
 import br.com.abstraindoBootcamp.desafio.dominio.Mentoria;
 
@@ -5,7 +6,6 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-
 
 
         Curso curso1 = new Curso();
@@ -22,6 +22,8 @@ public class Main {
         mentoria.setTitulo("Mentoria Individual");
         mentoria.setDescricao("Curso intensivo individual");
         mentoria.setData(LocalDate.now());
+
+        //Conteudo conteudo = new Curso(); quando usamos esse jeito de instanciar estamos usando o pilar de polimorfismo
 
         System.out.println(curso1);
         System.out.println(curso2);
